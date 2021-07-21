4ba7b0ef58c5799369a4be91bfab947a750792c5
Mossyfighting <builetrongphuoc@gmail.com>
2021-07-21 23:43:14 +0700

    add comments

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # stochastic_optimization
+- Although Particle swarm optimization (PSO) is a simple algorithm to implement, it produces good results. PSO was developed by Eberhart and Kennedy in early 90, it is a biologically inspired optimization following to ant/bee colonies, or fish schooling. PSO is being used in a wide range of applications from antenna design, electromagnetic  applications, to a machine learning problems. Like many others stochastic optimization, PSO is not ensured to provide the global minimum, but it does a excellent and challenging tasks on such problems as high dimensional, non-convex problems. In this very short tutorial, I’ll show PSO in its simple form and implement it using numpy in matrix form.
+
+- The basis PSO routine can be described as follows:
+
+    I. Initialize
+        1. Randomly initialize particle positions. 
+        2. Randomly initialize particle velocities.
+    II. Optimize
+        LOOP
+            1. Evaluate cost function at each particle position.
+            2. Update all particle velocities.
+            3. Update all particle positions.
+            4. Update local best.
+            5. Update global best.
+        UNTIL Terminate condition is met.
+- Test functions:
+\begin{equation}
+Cost = \sum_{i=1}^6 x_{i}^2,
+\end{equation}
+with the minimum value is 0 with all variables are zero.
--- pso_test.py
+++ pso_test.py
@@ -16,19 +16,19 @@ omega = 0.5
 
 # compute all particles
 def compute_cost(position_particle):
-  cost = np.sum(position_particle**2, axis = 1)
-  return cost
+    cost = np.sum(position_particle**2, axis = 1)
+    return cost
 
 def update_local_best(x_particle, x_local_best):
-  immediate_cost = compute_cost(x_particle)
-  local_cost = compute_cost(x_local_best)
-  mask = immediate_cost < local_cost
-  x_local_best[mask,:] = x_particle[mask, :]
-  return x_local_best
+    immediate_cost = compute_cost(x_particle)
+    local_cost = compute_cost(x_local_best)
+    mask = immediate_cost < local_cost
+    x_local_best[mask,:] = x_particle[mask, :]
+    return x_local_best
 
 def select_best_among_local_best(x_local_best):
-  cost = compute_cost(x_local_best)
-  return  x_particle[np.argmin(cost)], np.min(cost)
+    cost = compute_cost(x_local_best)
+    return  x_particle[np.argmin(cost)], np.min(cost)
 
 # update velocity
 def update_velocity(omega, 
@@ -36,16 +36,16 @@ def update_velocity(omega,
                     r_local, r_global, 
                     phi_local, phi_global, 
                     x_local_best, x_global_best):
-  # scale velocity 
-  _velocity_term = omega*v_particle
-  # local interaction
-  _local_term = phi_local*r_local*(x_local_best - x_particle)
-  # global interaction
-  _global_term = phi_global*r_global*(x_global_best - x_particle)
-  new_velocity = _velocity_term + _local_term + _global_term
-  return new_velocity
+    # scale velocity 
+    _velocity_term = omega*v_particle
+    # local interaction
+    _local_term = phi_local*r_local*(x_local_best - x_particle)
+    # global interaction
+    _global_term = phi_global*r_global*(x_global_best - x_particle)
+    new_velocity = _velocity_term + _local_term + _global_term
+    return new_velocity
 def update_pisition(x_particle, new_velocity, lr):
-  return x_particle + lr*new_velocity
+    return x_particle + lr*new_velocity
 
 # run the PSO
 n_iteration = 50
@@ -61,27 +61,27 @@ x_global_best = np.random.uniform(low = b_lo, high = b_up, size=(1, n_dimension)
 x_local_best = update_local_best(x_particle, x_local_best)
 entity_best, entity_cost = select_best_among_local_best(x_local_best)
 if compute_cost(x_global_best)[0] > entity_cost:
-  x_global_best = entity_best.reshape(1,n_dimension)
+    x_global_best = entity_best.reshape(1,n_dimension)
 # 3. generate velocity
 v_particle = np.random.uniform(low = b_lo , high = b_up, size = (n_particle, n_dimension))
 # 4. start the PSO
 for iter in range(n_iteration):
-  # 4.1 ----- update velocity
-  v_particle = update_velocity(omega, 
+    # 4.1 ----- update velocity
+    v_particle = update_velocity(omega, 
                                 v_particle, x_particle,
                                 r_local, r_global, 
                                 phi_local, phi_global, 
                                 x_local_best, x_global_best)
-  # 4.2 ----- update position
-  x_particle = update_pisition(x_particle, v_particle, lr)
-  x_particle = np.clip(x_particle, b_lo, b_up)
-  # 4.3 ----- update local best
-  x_local_best = update_local_best(x_particle, x_local_best)
-  x_local_best = np.clip(x_local_best, b_lo, b_up)
-  # 4.4 ----- update global best
-  x_entity_best, entity_cost = select_best_among_local_best(x_local_best)
-  if compute_cost(x_global_best)[0] > entity_cost:
-    x_global_best = x_entity_best.reshape(1,n_dimension)
-  global_cost.append(compute_cost(x_global_best)[0])
-  print("Iter: {}, cost: {}".format(iter, compute_cost(x_global_best)[0]))
+    # 4.2 ----- update position
+    x_particle = update_pisition(x_particle, v_particle, lr)
+    x_particle = np.clip(x_particle, b_lo, b_up)
+    # 4.3 ----- update local best
+    x_local_best = update_local_best(x_particle, x_local_best)
+    x_local_best = np.clip(x_local_best, b_lo, b_up)
+    # 4.4 ----- update global best
+    x_entity_best, entity_cost = select_best_among_local_best(x_local_best)
+    if compute_cost(x_global_best)[0] > entity_cost:
+        x_global_best = x_entity_best.reshape(1,n_dimension)
+    global_cost.append(compute_cost(x_global_best)[0])
+    print("Iter: {}, cost: {}".format(iter, compute_cost(x_global_best)[0]))
 print(x_global_best)
